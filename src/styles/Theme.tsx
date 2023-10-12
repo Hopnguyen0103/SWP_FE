103b54de6834549d40d6e01ee1eadb83b54ae7da
HOP NGUYEN <hopnvhe164017@fpt.edu.vn>
2023-10-12 19:52:41 +0700

    tha t di github oi

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // main: '#feAFA2',
+      main: "#3e5962"
+      // main: '#F6F9FC', 
+    },
+    secondary: {
+      main: "rgb(25, 118, 210)"
+    }
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+  },
+});
+
+export default theme;
