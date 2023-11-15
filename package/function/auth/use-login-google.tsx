e496a91cc9bb0daaff0754ad9a40b8b95db1c81d
17thaiduong <thaiduong0007@gmail.com>
2023-11-15 21:19:33 +0700

    fix

--- package/function/auth/use-login-google.tsx
+++ package/function/auth/use-login-google.tsx
@@ -1,9 +1,10 @@
 import { Auth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { FetcherProps, fetcher } from "../../fetcher";
-import { ResponseBody } from "../../model/api";
 import { UseLoginBody } from "../../model/api/auth/login";
-import { UseLoginGoogleBody } from "../../model/api/auth/loginGoogle";
+import { ResponseBody } from "../../model/api";
 import { User } from "../../model/user";
+import { UseLoginGoogleBody } from "../../model/api/auth/loginGoogle";
+
 
 export const UseLoginGoogle = async ({ auth, provider }: UseLoginGoogleBody<Auth>) => {
         const response = await signInWithPopup(auth, provider)
